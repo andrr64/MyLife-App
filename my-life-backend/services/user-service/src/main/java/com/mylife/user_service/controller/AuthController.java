e060c7fbcef68a8d50c5a967ceab9d0453ba9d90
Derza Andreas <derza.andreas@jubelio.com>
2025-11-08 23:05:33 +0700

    feat: add AuthController, LoginRequest, RegisterRequest, and AuthResponse classes

---
+++ my-life-backend/services/user-service/src/main/java/com/mylife/user_service/controller/AuthController.java
@@ -0,0 +1,5 @@
+package com.mylife.user_service.controller;
+
+public class AuthController {
+    
+}
---
+++ my-life-backend/services/user-service/src/main/java/com/mylife/user_service/dto/response/AuthResponse.java
@@ -0,0 +1,11 @@
+package com.mylife.user_service.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class AuthResponse {
+    private String accessToken;
+    private String refreshToken;
+}
