dd5e467d92b7cbbe7efffd367e49a9954342b5ff
Derza Andreas <derza.andreas@jubelio.com>
2025-11-08 23:58:29 +0700

    feat: enhance AuthController with user registration and login responses; add JWT configuration to application.yaml

--- my-life-backend/services/user-service/src/main/java/com/mylife/user_service/controller/AuthController.java
+++ my-life-backend/services/user-service/src/main/java/com/mylife/user_service/controller/AuthController.java
@@ -1,5 +1,35 @@
 package com.mylife.user_service.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mylife.user_service.dto.request.LoginRequest;
+import com.mylife.user_service.dto.request.RegisterRequest;
+import com.mylife.user_service.dto.response.AuthResponse;
+import com.mylife.user_service.service.AuthService;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/user-service/auth")
+@RequiredArgsConstructor
 public class AuthController {
-    
+    private final AuthService authService;
+
+    @PostMapping("/register")
+    public ResponseEntity<String> register(@Valid @RequestBody RegisterRequest request) {
+        authService.register(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body("User registered successfully");
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> login(@Valid @RequestBody LoginRequest request) {
+        AuthResponse response = authService.login(request);
+        return ResponseEntity.ok(response);
+    }
 }
