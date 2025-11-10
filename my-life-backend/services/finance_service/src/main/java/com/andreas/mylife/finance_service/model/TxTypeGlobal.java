447c61d1032cc9506eaa0415964eb152863aa72b
Derza Andreas <derza.andreas@jubelio.com>
2025-11-10 11:44:20 +0700

    feat(finance-service): add new entity "TxTypeGlobal"

---
+++ my-life-backend/services/finance_service/src/main/java/com/andreas/mylife/finance_service/model/TxTypeGlobal.java
@@ -0,0 +1,30 @@
+package com.andreas.mylife.finance_service.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "global_tx_type")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class TxTypeGlobal {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 64)
+    private String name;
+
+    @Column(nullable = false)
+    private Boolean effect;
+}
