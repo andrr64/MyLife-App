12716270b2364cdafc80af23aef29bbb1a418254
Derza Andreas <derza.andreas@jubelio.com>
2025-11-10 10:30:34 +0700

    feat(finance-service): add controllers (account, tx fams)

---
+++ my-life-backend/services/finance_service/src/main/java/com/andreas/mylife/finance_service/controller/AccountController.java
@@ -0,0 +1,12 @@
+package com.andreas.mylife.finance_service.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.andreas.mylife.finance_service.common.ApiPath;
+
+@RestController
+@RequestMapping(ApiPath.ACCOUNT)
+public class AccountController {
+
+}
