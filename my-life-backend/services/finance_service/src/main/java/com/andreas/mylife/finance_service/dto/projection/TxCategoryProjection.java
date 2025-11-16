77c4eb8ea931755e0a23769f75aa77b2c2f2ebc7
Derza Andreas <derza.andreas@jubelio.com>
2025-11-16 13:13:50 +0700

    feat: Remove TxType feature
    - Remove TxTypeController, TxTypeProjection, TxTypeRequest, TxTypeResponse, TxType, TxTypeGlobal, TxTypeRepository, TxTypeService and TxTypServiceImpl.
    - Feature is not needed anymore.

--- my-life-backend/services/finance_service/src/main/java/com/andreas/mylife/finance_service/dto/projection/TxCategoryProjection.java
+++
@@ -1,7 +0,0 @@
-package com.andreas.mylife.finance_service.dto.projection;
-
-public interface TxCategoryProjection {
-    Long getId();
-    String getName();
-    java.util.UUID getUserId();
-}
--- my-life-backend/services/finance_service/src/main/java/com/andreas/mylife/finance_service/dto/projection/TxTypeProjection.java
+++
@@ -1,8 +0,0 @@
-package com.andreas.mylife.finance_service.dto.projection;
-
-public interface TxTypeProjection {
-    Long getId();
-    String getName();
-    Boolean getEffect();
-    String getType(); // Tambahan untuk membedakan sumber data (Global / Personal)
-}
