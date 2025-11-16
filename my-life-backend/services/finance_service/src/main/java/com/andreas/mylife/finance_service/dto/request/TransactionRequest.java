49afe8baca6e5ec78d1fa97bfb776dc621d29967
Derza Andreas <derza.andreas@jubelio.com>
2025-11-16 09:30:16 +0700

    feat(finance-service): TransactionRequest DTO Validation and new Response DTOs
    - Added validation constraints to TransactionRequest DTO.
    - Created TransactionResponse and TxCategoryResponse DTOs.

--- my-life-backend/services/finance_service/src/main/java/com/andreas/mylife/finance_service/dto/request/TransactionRequest.java
+++ my-life-backend/services/finance_service/src/main/java/com/andreas/mylife/finance_service/dto/request/TransactionRequest.java
@@ -1,17 +1,21 @@
 package com.andreas.mylife.finance_service.dto.request;
 
 import java.math.BigDecimal;
-import java.util.UUID;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class TransactionRequest {
+    @NotBlank
+    @Size(max = 128, message = "Max 128 chars.")
+    private String title;
 
-    @NotNull
-    private UUID userId;
+    @NotNull(message = "Insert account id")
+    private Long accountId;
 
     @NotNull
     @Positive
@@ -19,4 +23,7 @@ public class TransactionRequest {
 
     @NotNull
     private Long categoryId;
+
+    @Size(max = 255)
+    private String description;
 }
---
+++ my-life-backend/services/finance_service/src/main/java/com/andreas/mylife/finance_service/dto/response/TransactionResponse.java
@@ -0,0 +1,6 @@
+package com.andreas.mylife.finance_service.dto.response;
+
+
+public class TransactionResponse {
+    
+}
