7d88246a2f56f49f85e51f89b09e94af46588a6a
Derza Andreas <derza.andreas@jubelio.com>
2025-11-17 16:21:56 +0700

    feat: Enable and configure CORS
    - Enables CORS in SecurityConfig.
    - Configures CORS to allow requests from http://localhost:3000 with specific methods, headers, and credentials.

--- my-life-backend/services/user-service/src/main/java/com/mylife/user_service/config/SecurityConfig.java
+++ my-life-backend/services/user-service/src/main/java/com/mylife/user_service/config/SecurityConfig.java
@@ -2,10 +2,16 @@ package com.mylife.user_service.config;
 
 import com.mylife.user_service.exception.CustomAuthenticationEntryPoint;
 import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @RequiredArgsConstructor
@@ -17,6 +23,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
+                .cors(cors -> {}) // ← AKTIFKAN CORS DI SINI
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/user-service/auth/**").permitAll()
                         .anyRequest().authenticated())
@@ -24,5 +31,18 @@ public class SecurityConfig {
                         .authenticationEntryPoint(customAuthenticationEntryPoint));
 
         return http.build();
+    }
+
+        @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(List.of("http://localhost:3000")); // frontend
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowCredentials(true); // wajib kalau pakai cookie/session
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
     }
 }
